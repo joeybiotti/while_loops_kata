01ba6bac1d12ebb54e51113e7bd00f2e8bd4f570
Joey Biotti <joey@MacBook-Pro-2.local>
2017-10-12 22:12:54 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace while_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 0;
+            while(value != 5)
+            {
+                Console.WriteLine("Enter the number 5: ");
+                string line = Console.ReadLine();
+
+                Int32.TryParse(line, out value);
+            }
+
+            Console.WriteLine("That's it!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# While Loops.
+
+Practice, Practice, Practice...
